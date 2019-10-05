434aebda717bcce6f5b5f2d9676e09f7835edf1c
SergioBarbosaX <serbarbos@hotmail.com>
2019-10-05 13:37:51 +0200

    Updates

---
+++ precache-manifest.a443be6640a4f10323c76214d5bfd905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98546934269e130a4efa3f86e0587e0a",
+    "url": "/cal-react/index.html"
+  },
+  {
+    "revision": "343718ed8b526198d84d",
+    "url": "/cal-react/static/css/main.e1d9adf5.chunk.css"
+  },
+  {
+    "revision": "6fde021c62ba52152b85",
+    "url": "/cal-react/static/js/2.7aa4910d.chunk.js"
+  },
+  {
+    "revision": "343718ed8b526198d84d",
+    "url": "/cal-react/static/js/main.49fbf5c7.chunk.js"
+  },
+  {
+    "revision": "78761a3bb93c0f4f8dd2",
+    "url": "/cal-react/static/js/runtime~main.49ce0728.js"
+  }
+]);
